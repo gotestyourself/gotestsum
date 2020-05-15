7a392378725d6ca2e658d890589d338a2d9cc20e
Daniel Nephin <dnephin@gmail.com>
2020-05-15 14:39:59 -0400

    Aggregate elapsed time when there are multiple runs
    A test case may have multiple runs if -count=x is used, or when the input contains the
    test2json output from multiple test runs.
    
    Using the median time should exclude some outliers.

--- cmd/tool/slowest/slowest.go
+++ cmd/tool/slowest/slowest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"os"
 	"sort"
 	"time"
@@ -122,7 +123,9 @@ func run(opts *options) error {
 // slowTestCases returns a slice of all tests with an elapsed time greater than
 // threshold. The slice is sorted by Elapsed time in descending order (slowest
 // test first).
-// FIXME: use medium elapsed time when there are multiple instances of the same test
+//
+// If there are multiple runs of a TestCase, all of them will be represented
+// by a single TestCase with the median elapsed time in the returned slice.
 func slowTestCases(exec *testjson.Execution, threshold time.Duration) []testjson.TestCase {
 	if threshold == 0 {
 		return nil
@@ -130,7 +133,8 @@ func slowTestCases(exec *testjson.Execution, threshold time.Duration) []testjson
 	pkgs := exec.Packages()
 	tests := make([]testjson.TestCase, 0, len(pkgs))
 	for _, pkg := range pkgs {
-		tests = append(tests, exec.Package(pkg).TestCases()...)
+		pkgTests := aggregateTestCases(exec.Package(pkg).TestCases())
+		tests = append(tests, pkgTests...)
 	}
 	sort.Slice(tests, func(i, j int) bool {
 		return tests[i].Elapsed > tests[j].Elapsed
@@ -139,6 +143,43 @@ func slowTestCases(exec *testjson.Execution, threshold time.Duration) []testjson
 		return tests[i].Elapsed < threshold
 	})
 	return tests[:end]
+}
+
+// collectTestCases maps all test cases by name, and if there is more than one
+// instance of a TestCase, finds the median elapsed time for all the runs.
+//
+// All cases are assumed to be part of the same package.
+func aggregateTestCases(cases []testjson.TestCase) []testjson.TestCase {
+	if len(cases) < 2 {
+		return cases
+	}
+	pkg := cases[0].Package
+	m := make(map[string][]time.Duration)
+	for _, tc := range cases {
+		m[tc.Test] = append(m[tc.Test], tc.Elapsed)
+	}
+	var result []testjson.TestCase
+	for name, timing := range m {
+		result = append(result, testjson.TestCase{
+			Package: pkg,
+			Test:    name,
+			Elapsed: median(timing),
+		})
+	}
+	return result
+}
+
+func median(times []time.Duration) time.Duration {
+	switch len(times) {
+	case 0:
+		return 0
+	case 1:
+		return times[0]
+	}
+	sort.Slice(times, func(i, j int) bool {
+		return times[i] < times[j]
+	})
+	return times[int(math.Ceil(float64(len(times)/2)))]
 }
 
 func jsonfileReader(v string) (io.ReadCloser, error) {
