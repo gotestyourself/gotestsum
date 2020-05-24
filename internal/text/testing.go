5cc60e2e225808fd34da77d9224c9d5768401c95
Daniel Nephin <dnephin@gmail.com>
2020-05-24 02:38:30 -0400

    Add an e2e test for rerun-fails
    Use -count=1 because there appears to be a bug in the 'go test' cache
    that has it PASS (cached) instead of failing when SEED=3.

---
+++ internal/text/testing.go
@@ -0,0 +1,42 @@
+package text
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+// ProcessLines from the Reader by passing each one to ops. The output of each
+// op is passed to the next. Returns the string created by joining all the
+// processed lines.
+func ProcessLines(t *testing.T, r io.Reader, ops ...func(string) string) string {
+	t.Helper()
+	out := new(strings.Builder)
+	scan := bufio.NewScanner(r)
+	for scan.Scan() {
+		line := scan.Text()
+		for _, op := range ops {
+			line = op(line)
+		}
+		out.WriteString(line + "\n")
+	}
+	assert.NilError(t, scan.Err())
+	return out.String()
+}
+
+func OpRemoveSummaryLineElapsedTime(line string) string {
+	if i := strings.Index(line, " in "); i > 0 {
+		return line[:i]
+	}
+	return line
+}
+
+func OpRemoveTestElapsedTime(line string) string {
+	if i := strings.Index(line, " (0.0"); i > 0 && i+8 == len(line) {
+		return line[:i]
+	}
+	return line
+}
---
+++ testdata/e2e/flaky/flaky_test.go
@@ -0,0 +1,67 @@
+// +build testdata
+
+package flaky
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+var seed int
+var seedfile = seedFile()
+var once = new(sync.Once)
+
+func setup(t *testing.T) {
+	once.Do(func() {
+		raw, err := ioutil.ReadFile(seedfile)
+		if err != nil {
+			t.Fatalf("failed to read seed: %v", err)
+		}
+		n, err := strconv.ParseInt(string(raw), 10, 64)
+		if err != nil {
+			t.Fatalf("failed to parse seed: %v", err)
+		}
+		seed = int(n)
+
+		err = ioutil.WriteFile(seedfile, []byte(strconv.Itoa(seed+1)), 0644)
+		if err != nil {
+			t.Fatalf("failed to write seed: %v", err)
+		}
+	})
+	fmt.Fprintln(os.Stderr, "SEED: ", seed)
+}
+
+func seedFile() string {
+	if name, ok := os.LookupEnv("TEST_SEEDFILE"); ok {
+		return name
+	}
+	return "/tmp/gotestsum-flaky-seedfile"
+}
+
+func TestAlwaysPasses(t *testing.T) {
+}
+
+func TestFailsRarely(t *testing.T) {
+	setup(t)
+	if seed%10 != 1 {
+		t.Fatal("not this time")
+	}
+}
+
+func TestFailsSometimes(t *testing.T) {
+	setup(t)
+	if seed%10 != 2 {
+		t.Fatal("not this time")
+	}
+}
+
+func TestFailsOften(t *testing.T) {
+	setup(t)
+	if seed%10 != 4 {
+		t.Fatal("not this time")
+	}
+}
