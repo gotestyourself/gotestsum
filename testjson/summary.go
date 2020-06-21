d7046046c1576b9c72e588046594932c5032f50e
Daniel Nephin <dnephin@gmail.com>
2020-06-20 20:09:21 -0400

    Remove PAUSE and CONT lines from summary output
    These lines are necessary when the tests are running in parallel, but when the
    summary is being printed all the tests are complete, so they are only noise.

--- testjson/summary.go
+++ testjson/summary.go
@@ -174,7 +174,7 @@ func writeTestCaseSummary(out io.Writer, execution executionSummary, conf testCa
 			tc.Test,
 			FormatDurationAsSeconds(tc.Elapsed, 2))
 		for _, line := range execution.OutputLines(tc) {
-			if isRunLine(line) || conf.filter(tc.Test, line) {
+			if isFramingLine(line) || conf.filter(tc.Test, line) {
 				continue
 			}
 			fmt.Fprint(out, line)
@@ -218,6 +218,8 @@ func formatSkipped() testCaseFormatConfig {
 	}
 }
 
-func isRunLine(line string) bool {
-	return strings.HasPrefix(line, "=== RUN   Test")
+func isFramingLine(line string) bool {
+	return strings.HasPrefix(line, "=== RUN   Test") ||
+		strings.HasPrefix(line, "=== PAUSE Test") ||
+		strings.HasPrefix(line, "=== CONT  Test")
 }
