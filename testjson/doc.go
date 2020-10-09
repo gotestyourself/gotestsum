493610cfc6e59a591316434365e0700a51f780a9
Daniel Nephin <dnephin@gmail.com>
2020-10-09 01:11:40 -0400

    testjson: improve example in overview.

--- testjson/doc.go
+++ testjson/doc.go
@@ -3,13 +3,18 @@ Events are passed to a formatter for output.
 
 Example
 
-This example reads the test2json output from os.Stdin. It builds an
-Execution from the output, then it prints the number of tests run.
+This example reads the test2json output from os.Stdin. It sends every
+event to the handler, builds an Execution from the output, then it
+prints the number of tests run.
 
-
-    exec, err := testjson.ScanTestOutput(testjson.ScanConfig{Stdout: os.Stdin})
+    exec, err := testjson.ScanTestOutput(testjson.ScanConfig{
+        // Stdout is a reader that provides the test2json output stream.
+        Stdout: os.Stdin,
+        // Handler receives TestEvents and error lines.
+        Handler: eventHandler,
+    })
     if err != nil {
-        return fmt.Errorf("failed to scan testjson: %v", err)
+        return fmt.Errorf("failed to scan testjson: %w", err)
     }
     fmt.Println("Ran %d tests", exec.Total())
 
