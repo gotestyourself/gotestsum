13149aa25bd2f8f124320ef5037f85d6b00ddfc2
Mattias de Zalenski <zalenski@northvolt.com>
2023-04-25 04:09:32 +0200

    Add dots summary in pkgname-compact
    Add -dots[N] to --format-compact to add dots summary

--- testjson/compactformat.go
+++ testjson/compactformat.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -15,7 +16,7 @@ import (
 	"golang.org/x/term"
 )
 
-const CompactFormats = "relative, short, partial, partial-back"
+const CompactFormats = "relative, short, partial, partial-back, -dots"
 
 func CompactFormatUsage(out io.Writer, name string) {
 	fmt.Fprintf(out, `
@@ -24,6 +25,7 @@ Formats:
 	short                    print the last path segment of the package
 	partial                  print newly entered path segments for each package
 	partial-back             partial with an indication when it backs out
+	-dots[N]                 print test dots summary after the package
 
 `)
 }
@@ -40,10 +42,12 @@ type pkgLine struct {
 	path        string
 	event       TestEvent
 	lastElapsed time.Duration
+	dots        []string
 }
 
 func shouldJoinPkgs(opts FormatOptions, lastPkg, pkg string) (join bool, commonPrefix string, backUp int) {
-	switch opts.CompactPkgNameFormat {
+	pkgNameFormat := dotFmtRe.ReplaceAllString(opts.CompactPkgNameFormat, "")
+	switch pkgNameFormat {
 	case "relative":
 		return true, "", 0
 	case "short":
@@ -64,7 +68,10 @@ func shouldJoinPkgs(opts FormatOptions, lastPkg, pkg string) (join bool, commonP
 
 func pkgNameCompactFormat(out io.Writer, opts FormatOptions) eventFormatterFunc {
 	buf := bufio.NewWriter(out)
-	pt := &PkgTracker{opts: opts}
+	pt := &PkgTracker{
+		opts: opts,
+		pkgs: map[string]*pkgLine{},
+	}
 
 	w, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil || w == 0 {
@@ -72,7 +79,25 @@ func pkgNameCompactFormat(out io.Writer, opts FormatOptions) eventFormatterFunc 
 	}
 
 	return func(event TestEvent, exec *Execution) error {
+		pkgPath := RelativePackagePath(event.Package)
+
+		p := pt.pkgs[pkgPath]
+		if p == nil {
+			p = &pkgLine{
+				path:  pkgPath,
+				event: event,
+			}
+			pt.pkgs[pkgPath] = p
+		}
+
 		if !event.PackageEvent() {
+			var dot string
+			if dotFmtRe.MatchString(opts.CompactPkgNameFormat) {
+				dot = fmtDot(event)
+				if dot != "" {
+					p.dots = append(p.dots, dot)
+				}
+			}
 			if event.Action == ActionFail && opts.OutputTestFailures {
 				if pt.col > 0 {
 					buf.WriteString("\n")
@@ -86,12 +111,11 @@ func pkgNameCompactFormat(out io.Writer, opts FormatOptions) eventFormatterFunc 
 			return nil
 		}
 
-		pkgPath := RelativePackagePath(event.Package)
-
 		eventStr := strings.TrimSuffix(shortFormatPackageEvent(opts, event, exec), "\n")
 		if eventStr == "" {
 			return nil
 		}
+		eventStr += dotSummary(p.dots, dotFmtRe.FindString(opts.CompactPkgNameFormat))
 
 		pt.writeEventStr(pkgPath, eventStr, event, w, buf, exec.Elapsed())
 		return buf.Flush()
@@ -145,6 +169,7 @@ func (pt *PkgTracker) compactEventStr(pkgPath string, eventStr string, event Tes
 	return eventStr, join
 }
 
+var dotFmtRe = regexp.MustCompile(`-?dots([0-9]+)?`)
 var colorRe = regexp.MustCompile(`\x1b\[[0-9;]*m`)
 
 func noColorLen(s string) int {
@@ -158,15 +183,34 @@ func noColorLen(s string) int {
 // ---
 
 func pkgNameCompactFormat2(out io.Writer, opts FormatOptions) eventFormatterFunc {
-	pkgTracker := &PkgTracker{
+	pt := &PkgTracker{
 		opts: opts,
 		pkgs: map[string]*pkgLine{},
 	}
 
 	writer := dotwriter.New(out)
+	lastNonEventFlush := time.Now()
 
 	return func(event TestEvent, exec *Execution) error {
+		pkgPath := RelativePackagePath(event.Package)
+
+		p := pt.pkgs[pkgPath]
+		if p == nil {
+			p = &pkgLine{
+				path:  pkgPath,
+				event: event,
+			}
+			pt.pkgs[pkgPath] = p
+		}
+
 		if !event.PackageEvent() {
+			var dot string
+			if dotFmtRe.MatchString(opts.CompactPkgNameFormat) {
+				dot = fmtDot(event)
+				if dot != "" {
+					p.dots = append(p.dots, dot)
+				}
+			}
 			if event.Action == ActionFail && opts.OutputTestFailures {
 				pkg := exec.Package(event.Package)
 				tc := pkg.LastFailedByName(event.Test)
@@ -178,27 +222,26 @@ func pkgNameCompactFormat2(out io.Writer, opts FormatOptions) eventFormatterFunc
 				writer = dotwriter.New(out)
 				// continue to mark the package as failed early
 			} else {
+				if dot != "" {
+					if time.Since(lastNonEventFlush) < 50*time.Millisecond {
+						return nil
+					}
+					lastNonEventFlush = time.Now()
+					return pt.flush(writer, opts, exec)
+				}
 				return nil
 			}
 		}
-
-		pkgPath := RelativePackagePath(event.Package)
+		p.lastElapsed = exec.Elapsed()
 
 		// Remove newline from shortFormatPackageEvent
 		eventStr := strings.TrimSuffix(shortFormatPackageEvent(opts, event, exec), "\n")
 		if eventStr == "" {
-			if p := pkgTracker.pkgs[pkgPath]; p != nil {
-				p.lastElapsed = exec.Elapsed()
-			}
-			return pkgTracker.flush(writer, opts, exec)
+			return pt.flush(writer, opts, exec)
 		}
 
-		pkgTracker.pkgs[pkgPath] = &pkgLine{
-			path:        pkgPath,
-			event:       event,
-			lastElapsed: exec.Elapsed(),
-		}
-		return pkgTracker.flush(writer, opts, exec)
+		p.event = event
+		return pt.flush(writer, opts, exec)
 	}
 }
 
@@ -263,12 +306,20 @@ func (pt *PkgTracker) flush(writer *dotwriter.Writer, opts FormatOptions, exec *
 		for i, pkg := range pkgs {
 			event := pkg.event
 			eventStr := strings.TrimSuffix(shortFormatPackageEvent(opts, event, exec), "\n")
+			if dots := dotSummary(pkg.dots, dotFmtRe.FindString(opts.CompactPkgNameFormat)); dots != "" {
+				if eventStr == "" {
+					eventStr = pkg.path
+				}
+				eventStr += dots
+			}
 			compactStr, join := pt.compactEventStr(pkg.path, eventStr, event, w)
 			if !join || i == 0 {
 				flushLine()
 				pt.col += wallTimeCol
 			}
-			parts = append(parts, compactStr)
+			if compactStr != "" {
+				parts = append(parts, compactStr)
+			}
 			if pkg.lastElapsed > elapsed {
 				elapsed = pkg.lastElapsed
 			}
@@ -279,4 +330,43 @@ func (pt *PkgTracker) flush(writer *dotwriter.Writer, opts FormatOptions, exec *
 	buf.Flush() // nolint:errcheck
 	PrintSummary(writer, exec, SummarizeNone)
 	return writer.Flush()
+}
+
+func dotSummary(dots []string, dotFmt string) string {
+	var limit = 1
+	if nstr := strings.TrimLeft(dotFmt, "-dots"); nstr != "" {
+		if n, err := strconv.Atoi(nstr); err == nil {
+			limit = n
+		}
+	}
+	if len(dots) > limit {
+		sort.Strings(dots)
+	}
+	var s string
+	var prev string
+	var count int
+	add := func() {
+		if count == 0 {
+			return
+		}
+		if count <= limit || (s == "" && count <= limit+3) {
+			s += strings.Repeat(prev, count)
+		} else {
+			if s == "" {
+				s += strings.Repeat(prev, limit-1)
+			}
+			reset := "\x1b[0m"
+			s += fmt.Sprintf("%s[%d]%s", strings.TrimSuffix(prev, reset), count, reset)
+		}
+	}
+	for _, dot := range dots {
+		if dot != prev {
+			add()
+			prev = dot
+			count = 0
+		}
+		count++
+	}
+	add()
+	return s
 }
